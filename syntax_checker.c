3a444c67a65d60dae81e03b12b389a99e22cb72e
chudyandrej <xchudy03@stud.fit.vutbr.cz>
2015-10-15 21:04:40 +0200

    main scope

--- syntax_checker.c
+++ syntax_checker.c
@@ -2,6 +2,7 @@
 // Created by andrej on 15.10.2015.
 //
 
+#include <stdbool.h>
 #include "syntax_checker.h"
 
 int start_syntax_analyz(){
@@ -24,9 +25,8 @@ int dec_funcion(){
                 if (strcmp(next_token()->Lexeme, ")")){
                     token_stract* token = next_token();
                     if (strcmp(token->Lexeme, ";")){
-                        return 0;
-                    }
-                    else if(strcmp(token->Lexeme, "{"){
+                        return 0;}
+                    else if(strcmp(token->Lexeme, "{")){
                         return body_funcion();
                     }else
                         return 1;
@@ -35,8 +35,57 @@ int dec_funcion(){
         }
     }
     return 1;
+}
+int command(){
+
+    char *catedory = next_token()->Lexeme;
+    if (strcmp(catedory, "cin")) { }
+    else if(strcmp(catedory, "cout")){ }
+    else if(strcmp(catedory, "for")){    }
+    else if(strcmp(catedory, "if")){    }
+    else if(strcmp(catedory, "sort")){ return one_par_command(); }
+    else if(strcmp(catedory, "find")){ return two_par_command(); }
+    else if(strcmp(catedory, "concat")){ return two_par_command(); }
+    else if(strcmp(catedory, "substr")){ return three_par_command(); }
+    else if(strcmp(catedory, "length")){ return one_par_command(); }
+    else if(strcmp(catedory, "return")){ return one_par_command(); }
+
+    return 1;
 
 }
+
+
+int assing(){
+
+}
+int dec_variable(){
+
+}
+
+
+
+
+int body_funcion(){
+    while(true) {
+        char *catedory = next_token()->category;
+        if (strcmp(catedory, "command")) {
+            if (command() == 0){ continue; } else{ return 1; }
+        }
+        else if(strcmp(catedory, "id")){
+            if (assing() == 0){ continue; } else{ return 1; }
+        }
+        else if(strcmp(catedory, "type")){
+            if (dec_variable() == 0) { continue; } else { return 1; }
+        }
+        else if(strcmp(catedory, "}")){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+}
+
 
 
 int check_type(const char *lexem){
@@ -61,7 +110,7 @@ int check_type(const char *lexem){
 
 int var_declaration(void){
     
-    token_stract token* = next_token();  /*add to queue?*/
+    token_stract *token = next_token();  /*add to queue?*/
     if (token->category != "id") return 1;    
     
     token = next_token();               /*add to queue?*/
@@ -76,13 +125,13 @@ int var_declaration(void){
     }
 }
 
-int parameters(void){
-    token_stract token* = next_token();  /*add to queue?*/
+int parameters(){
+    token_stract *token = next_token();  /*add to queue?*/
      
-    if(type(token->Lexeme)) return 1;           /*type returns 1 when error*/
+    if(check_type(token->Lexeme)) return 1;           /*type returns 1 when error*/
     
     token = next_token();               /*add to queue?*/
-    if(token.category != "id") return 1;
+    if(token->category != "id") return 1;
     
     token = next_token();               /*add to queue?*/
     if(token->Lexeme == ")") return 0;
@@ -92,4 +141,55 @@ int parameters(void){
     }
     return 0;
     
+}
+
+int one_par_command(){
+    if (strcmp(next_token()->Lexeme, "(")){
+        if (strcmp(next_token()->category, "id")){
+            if (strcmp(next_token()->Lexeme, ")")){
+                if (strcmp(next_token()->Lexeme, ";")){ //gun
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int two_par_command(){
+    if (strcmp(next_token()->Lexeme, "(")){
+        if (strcmp(next_token()->category, "id")) {
+            if (strcmp(next_token()->category, ",")) {
+                if (strcmp(next_token()->category, "id")) {
+                    if (strcmp(next_token()->Lexeme, ")")) {
+                        if (strcmp(next_token()->Lexeme, ";")) {
+                            return 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int three_par_command(){
+    if (strcmp(next_token()->Lexeme, "(")){
+        if (strcmp(next_token()->category, "id")) {
+            if (strcmp(next_token()->category, ",")) {
+                if (strcmp(next_token()->category, "id")) {
+                    if (strcmp(next_token()->category, ",")) {
+                        if (strcmp(next_token()->category, "id")) {
+                            if (strcmp(next_token()->Lexeme, ")")) {
+                                if (strcmp(next_token()->Lexeme, ";")) {
+                                    return 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 1;
 }
--- syntax_checker.h
+++ syntax_checker.h
@@ -7,5 +7,16 @@
 
 #include "fake_skener.h"
 
-
+int start_syntax_analyz();
+int dec_funcion();
+int check_type(const char *lexem);
+int var_declaration(void);
+int parameters();
+int body_funcion();
+int command();
+int assing();
+int dec_variable();
+int one_par_command();
+int two_par_command();
+int three_par_command();
 #endif //INTERPRET_SYNTAX_CHECKER_H
