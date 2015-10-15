b2abd736e746c883e4e05132a5f97b0a4eb5e35f
chudyandrej <xchudy03@stud.fit.vutbr.cz>
2015-10-15 18:27:49 +0200

    declaration funcion lexical chack

--- syntax_checker.c
+++ syntax_checker.c
@@ -3,3 +3,58 @@
 //
 
 #include "syntax_checker.h"
+
+int start_syntax_analyz(){
+
+    token_stract* token  = next_token();
+    if (check_type(token->Lexeme) == 0){
+        return (dec_funcion() == 0)? 0:1;
+    }else{
+
+        return 1;
+
+    }
+}
+
+int dec_funcion(){
+
+    if (strcmp(next_token()->category, "id")){
+        if (strcmp(next_token()->Lexeme, "(")){
+            if(parameters() == 0){
+                if (strcmp(next_token()->Lexeme, ")")){
+                    token_stract* token = next_token();
+                    if (strcmp(token->Lexeme, ";")){
+                        return 0;
+                    }
+                    else if(strcmp(token->Lexeme, "{"){
+                        return body_funcion();
+                    }else
+                        return 1;
+                }
+            }
+        }
+    }
+    return 1;
+
+}
+
+
+int check_type(const char *lexem){
+
+    if (strcmp(lexem, "int") == 0){
+        return 0;
+    }
+    else if(strcmp(lexem, "double") == 0){
+        return 0;
+    }
+    else if(strcmp(lexem, "auto") == 0){
+        return 0;
+    }
+    else if(strcmp(lexem, "string") == 0){
+        return 0;
+    }
+    else {
+        return 1;
+    }
+
+}
--- syntax_checker.h
+++ syntax_checker.h
@@ -5,6 +5,7 @@
 #ifndef INTERPRET_SYNTAX_CHECKER_H
 #define INTERPRET_SYNTAX_CHECKER_H
 
+#include "fake_skener.h"
 
 
 #endif //INTERPRET_SYNTAX_CHECKER_H
