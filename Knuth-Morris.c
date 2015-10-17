cc645150a1f023635d3f9fb8278ddd4d9621e605
batyskaf684 <batyskaf684@bitbucket.org>
2015-10-17 16:14:12 +0200

    Knuth-Morris algorithm implemented for find function

---
+++ Knuth-Morris.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/*Knuth-Morris algorithm for string matching*/
+
+int find(char *s, char *search){       
+    int pattern_length = strlen(search);    
+    
+    if (pattern_length == 0) { return 0; }
+    
+    int table[pattern_length];
+    table[0] = 0;       /*must be zero*/
+    int i=0; 
+    int j=0; 
+    
+    /*fill table with values*/
+    for(j = 1; j < pattern_length; j++){
+        if(search[j] == search[i]){
+            i++;
+            table[j] = i;
+        }
+        else{
+            while(i > 0 && search[i] != search[j]){
+                i--;
+            }
+            if(i == 0 &&  search[i] == search[j]){
+                table[j] = 1;
+                i = 1;
+            }
+            else{
+                table[j]=i;
+            }     
+        }
+    }
+    
+    
+    int source_length=strlen(s);
+    j=0;
+    
+    for(i=0; i < source_length; i++){
+        if(search[j] ==  s[i]){
+            j++;
+            if(j==pattern_length){      /*found search in s*/
+                return i-j+1;
+            }
+        }
+        else{
+            if(j==0) continue;      /*go again and compare next char of string with pattern*/
+            j=table[j-1];           /*save number according the table to j and */
+            i--;                    /*compare again the same char of string with new char of pattern*/
+        }        
+    }    
+    
+    return -1;    
+}
+
+int main(){
+    char *a=malloc(sizeof(char));
+    char *b=malloc(sizeof(char));
+
+    strcpy(a, "abrakasabraabrakadabra");
+    strcpy(b, "abrakadabra");
+    printf("%d\n", find(a,b));
+    
+    return 0;
+}
