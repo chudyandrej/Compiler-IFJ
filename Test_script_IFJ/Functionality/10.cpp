9df7d6fda604def697a3c2b7b266b401ffccf7a0
batyskaf684 <batyskaf684@bitbucket.org>
2015-11-12 18:02:05 +0100

    more tests added

---
+++ Test_script_IFJ/Functionality/10.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+
+int factorial( int n) {
+    int temp_result;
+    auto decremented_n = n - 1;
+   
+    if (n < 2) {
+        return 1;
+    } 
+    else {
+        temp_result = factorial(decremented_n);
+        return n * temp_result;
+    }
+}
+
+int main() {
+    int vysl;
+
+    vysl = factorial(5);
+    cout << vysl << "\n";
+
+    return 0;
+}
+
