4698006f8601353f7a4bbedc36264561579a74f5
batyskaf684 <batyskaf684@bitbucket.org>
2015-11-16 19:42:56 +0100

    tester and tests for parser

--- test_expression_process.sh
+++ test_expression_process.sh
@@ -7,18 +7,19 @@ while read p; do
     echo "\n\n----------------test----------------"
     i=$((i+1))  
 
+    echo "test line input no. $i: $p      #####"
+    echo "int main(){" > testforinterpret.txt
     echo $p >> testforinterpret.txt
-
+    echo "}" >> testforinterpret.txt
     ./$1 testforinterpret.txt
  
-    if [ $? -neq 0 ]; then 
-        echo "\nproblem na riadku" $i
+    if [ $? -ne $3 ]; then 
+        printf "\nproblem on line $i\n"
         rm testforinterpret.txt
         exit 1
     fi
-    
     done < $2;
 
-    echo "\n\n------------------------------"
-    echo "\nEverything OK lucky man!!!\n"
+    printf "\n\n------------------------------"
+    printf "\nEverything OK lucky man!!!\n"
     rm testforinterpret.txt
