7f760f819e78901aa345d0a3adf916220407fedb
batyskaf684 <batyskaf684@bitbucket.org>
2015-11-21 15:27:28 +0100

    test which ended well

---
+++ test_expression_process.sh
@@ -0,0 +1,25 @@
+#bin/sh
+
+
+i=0;
+
+while read p; do 
+    printf "\n\n----------------test----------------"
+    i=$((i+1))  
+
+    echo "test line input no. $i: $p      #####"
+    echo "int main(){" > testforinterpret.txt
+    echo $p >> testforinterpret.txt
+    echo "}" >> testforinterpret.txt
+    ./$1 testforinterpret.txt
+ 
+    if [ $? -ne $3 ]; then 
+        printf "\nproblem on line $i\n"
+        rm testforinterpret.txt
+        exit 1
+    fi
+    done < $2;
+
+    printf "\n\n------------------------------"
+    printf "\nEverything OK lucky man!!!\n"
+    rm testforinterpret.txt
