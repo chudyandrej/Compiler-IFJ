22cbbaf144b93641515a0da7de9ae4247b30ace3
Maros Kopec <xkopec44@stud.fit.vutbr.cz>
2015-11-26 16:01:02 +0100

    Updated main file

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "parser.h"
+#include "err.h"
 
 FILE *fp;
-
 Token *token_predict = NULL;
 struct tBST Func;
 tDLList *tac_stack;
@@ -11,14 +11,28 @@ void foobar(struct Operation * rec);
 void print_instuctions();
 
 int main(int argc, char *argv[]) {
-    printf("%d\n", argc);
+    
+    if (argc == 1)
+    {
+        errorMessage_internal("No argument");
+        return INTER_ERR;
+    }
+    else if (argc > 2)
+    {
+        errorMessage_internal("Too many arguments");
+        return INTER_ERR;
+    }
+    
     BSTInit(&Func);
-    if((garbage= malloc(sizeof(tDLList))) == NULL){
+    if((garbage = malloc(sizeof(tDLList))) == NULL){
         errorMessage_internal("Malloc garbage collector");
-        return 1;
+        return INTER_ERR;
     }
     init_list(garbage);
-    fp = fopen(argv[1],"r");
+    if ((fp = fopen(argv[1],"r")) == NULL ) {
+        errorMessage_internal("Can not open file. Make sure the path is right, or that you have rights to read");
+        return INTER_ERR;
+    }
     token_predict = get_token(fp);
     int exit_code = start_syntax_analyz();
 
